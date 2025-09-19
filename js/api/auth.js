2c287b14002c4e1e41f757e0a12d6d583ad5cf74
adring93 <adring@live.com>
2025-09-19 05:03:34 +0200

    Update auth.js

--- js/api/auth.js
+++ js/api/auth.js
@@ -1,6 +1,16 @@
 import { AUTH_URL } from './config.js';
 import { http } from './http.js';
+
 export async function registerUser(payload) {
+  return http(`${AUTH_URL}/register`, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+  });
 }
+
 export async function loginUser(payload) {
+  return http(`${AUTH_URL}/login`, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+  });
 }
